df4ed54b8acc9cd5820bbf212e7042a6c8d5e8c6
Hersh Patel <hershe.91@gmail.com>
2017-04-09 20:33:12 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match (array)
+    new_word = @word.split("").sort
+    array.select do |x|
+      x.split("").sort == new_word
+    end
+  end
+
+
+
+
+
+end
